35c0123e11ca1cd8fac8433f445f58537d79a21a
dhirensr <dhirensr@gmail.com>
2019-02-05 22:33:27 +0100

    added csv data and python code for analyzing the data.

---
+++ steel_data_analysis.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import sklearn as sk
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('/home/dhirensr/Desktop/steel_data.csv')
+
+sns.countplot(x="Class", data= df)
+
+
+
+df_class1 = df[df.Class==1]
+df_class2 = df[df.Class==2]
+df_class2_upsampled = sk.utils.resample(df_class2,
+                                        replace=True,
+                                        n_samples=1268)
+df = pd.concat([df_class1, df_class2_upsampled])
+print(len(df))
+sns.countplot(x="Class", data= df)
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(df.iloc[:,:-1], df['Class'], test_size=0.3)
+
+clf = svm.SVC(kernel='linear', C = 1.0)
+clf.fit(X_train,y_train)
+clf.predict([X_test,y_test])
+
+# w = clf.coef_[0]
+# print(w)
+
+# a = -w[0] / w[1]
+
+# xx = np.linspace(0,12)
+# yy = a * xx - clf.intercept_[0] / w[1]
+
+# h0 = plt.plot(xx, yy, 'k-', label="non weighted div")
+
+# plt.scatter(X[:, 0], X[:, 1], c = y)
+# plt.legend()
+# plt.show()
