4e08d064fc3845a7510a98f9c8f0551497eedcee
dhirensr <dhirensr@gmail.com>
2019-02-15 23:07:46 +0100

    modified the dashboard and also the split of data and models

--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,15 @@
 import dash
 from dash.dependencies import Input, Output
 import dash_html_components as html
+
+
 import dash_core_components as dcc
 import steel_data_analysis
 import datetime
+
+cnf,accuracy,test_count,pred_count=[[0,0]],0,[0,0,0],[0,0,0]
+cnf_knn,accuracy_knn,test_count_knn,pred_count_knn=[[0,0]],0,[0,0,0],[0,0,0]
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -16,15 +22,51 @@ app.layout = html.Div([
 html.Label('K neighbors'),
     dcc.Slider(id='rate', value=5, min=0, max=24, step=1),
 
+html.Div(id='amount-dummy'),
+html.Div(id='amount_dummy'),
+
+html.Label('Select Attributes'),
+    dcc.Checklist(id='attributes',
+    options=[
+        {'label': 'X_Minimum', 'value': 'V1'},
+        {'label': 'X_Maximum', 'value': 'V2'},
+        {'label': 'Y_Minimum ', 'value': 'V3'},
+        {'label': 'Y_Maximum ', 'value': 'V4'},
+        {'label': "Pixels_Areas", 'value': 'V5'},
+        {'label': 'X_Perimeter', 'value': 'V6'},
+        {'label': 'Y_Perimeter', 'value': 'V7'},
+        {'label': 'Sum_of_Luminosity ', 'value': 'V8'},
+        {'label': 'Minimum_of_Luminosity ', 'value': 'V9'},
+        {'label': 'Maximum_of_Luminosity', 'value': 'V10'},
+        {'label': 'Length_of_Conveyer', 'value': 'V11'},
+        {'label': 'TypeOfSteel_A300 ', 'value': 'V12'},
+        {'label': 'TypeOfSteel_A400', 'value': 'V13'},
+        {'label': 'Steel_Plate_Thickness', 'value': 'V14'},
+        {'label': 'Edges_Index', 'value': 'V15'},
+        {'label': 'Empty_Index', 'value': 'V16'},
+        {'label': 'Square_Index', 'value': 'V17'},
+        {'label': 'Outside_X_Index', 'value': 'V18'},
+        {'label': 'Edges_X_Index', 'value': 'V19'},
+        {'label': 'Edges_Y_Index', 'value': 'V20'},
+        {'label': 'Outside_Global_Index', 'value': 'V21'},
+        {'label': 'LogOfAreas', 'value': 'V22'},
+        {'label': 'Log_X_Index', 'value': 'V23'},
+        {'label': "Log_Y_Index", 'value': 'V24'},
+        {'label': 'Orientation_Index ', 'value': 'V25'},
+        {'label': 'Luminosity_Index ', 'value': 'V26'},
+        {'label': 'SigmoidOfAreas', 'value': 'V27'}
+
+    ],
+    values=['V1', 'V2']
+    ),
 html.Label('RandomForestClassifier Accuracy'),
     html.Div(id='amount'),
-
 
 html.Label('KNN Accuracy'),
     html.Div(id='amount-per-week'),
 html.Label('Heatmap-RandomForestClassifier'),
     html.Div(id='confusion_matrix'),
-html.Label('Heatmap- KNN Confusion'),
+    html.Label('Heatmap- KNN Confusion'),
     html.Div(id='confusion_matrix_1'),
 
 ###################
@@ -46,23 +88,46 @@ html.Label('Predicted_Results-KNN Bar'),
 
 ])
 
+@app.callback(Output('amount-dummy', 'children'),
+              [Input('attributes', 'values'),Input('hours', 'value')])
+def compute_amount(attributes,hours):
+    global cnf,accuracy,test_count,pred_count
+    cnf,accuracy,test_count,pred_count=steel_data_analysis.RandomForest(attributes,hours)
+    return ""
+
+@app.callback(Output('amount_dummy', 'children'),
+              [Input('attributes', 'values'),Input('rate', 'value')])
+def compute_amount(attributes,rate):
+    global cnf_knn,accuracy_knn,test_count_knn,pred_count_knn
+    cnf_knn,accuracy_knn,test_count_knn,pred_count_knn=steel_data_analysis.Kneighbors(attributes,rate)
+    return ""
+
+
+@app.callback(Output('amount-per-week', 'children'),
+              [Input('attributes', 'values'),Input('rate', 'value')])
+def compute_amount(attributes,rate):
+    global cnf_knn,accuracy_knn,test_count_knn,pred_count_knn
+    return accuracy_knn
+
 @app.callback(Output('amount', 'children'),
-              [Input('hours', 'value')])
-def compute_amount(hours):
-    return steel_data_analysis.RandomForest(hours)[1]
+              [Input('attributes', 'values'),Input('hours', 'value')])
+def compute_amount(attributes,hours):
+    global cnf,accuracy,test_count,pred_count
+    return accuracy
+
+
 
 @app.callback(Output('confusion_matrix', 'children'),
-              [Input('rate', 'value')])
-def heatmap_randomforest(rate):
+              [Input('attributes', 'values'),Input('hours', 'value')])
+def heatmap_randomforest(attributes,hours):
+    global cnf,accuracy,test_count,pred_count
     return html.Div([dcc.Graph(
         id='graph-1-tabs',
         figure={
             'data': [{
-                'z':  steel_data_analysis.RandomForest(rate)[0],
-                'text': [
-                    ['Pastry', 'Z-scratch', 'K_Scratch',"Stains",'Dirtiness','Bumps','Other_faults'],
-                    ['Pastry', 'Z-scratch', 'K_Scratch',"Stains",'Dirtiness','Bumps','Other_faults']
-                ],
+                'z':  cnf,
+                'x': ['Pastry', 'Z-scratch', 'K_Scratch',"Stains",'Dirtiness','Bumps','Other_faults'],
+                'y' :['Pastry', 'Z-scratch', 'K_Scratch',"Stains",'Dirtiness','Bumps','Other_faults'],
                 'type': 'heatmap',
                 "name" : "temperature"
             }]
@@ -70,32 +135,32 @@ def heatmap_randomforest(rate):
     )])
 
 @app.callback(Output('confusion_matrix_1', 'children'),
-              [Input('rate', 'value')])
-def heatmap_knn(rate):
+              [Input('attributes', 'values'),Input('rate', 'value')])
+def heatmap_knn(attributes,rate):
+    global cnf_knn,accuracy_knn,test_count_knn,pred_count_knn
     return html.Div([dcc.Graph(
         id='graph-2-tabs',
         figure={
             'data': [{
-                'z':  steel_data_analysis.Kneighbors(rate)[0],
-                'text': [
-                    ['Pastry', 'Z-scratch', 'K_Scratch',"Stains",'Dirtiness','Bumps','Other_faults'],
-                    ['Pastry', 'Z-scratch', 'K_Scratch',"Stains",'Dirtiness','Bumps','Other_faults']
-                ],
+                'z':  cnf_knn,
+                'x': ['Pastry', 'Z-scratch', 'K_Scratch',"Stains",'Dirtiness','Bumps','Other_faults'],
+                'y' :['Pastry', 'Z-scratch', 'K_Scratch',"Stains",'Dirtiness','Bumps','Other_faults'],
                 'type': 'heatmap',
-                "name" : "temperature"
+                "name" : "temperature-1"
             }]
         }
     )])
 
 
 @app.callback(Output('TR_RF', 'children'),
-              [Input('rate', 'value')])
-def bar_randomforest_test(rate):
+              [Input('attributes', 'values'),Input('hours', 'value')])
+def bar_randomforest_test(attributes,rate):
+    global cnf,accuracy,test_count,pred_count
     return html.Div([dcc.Graph(
         id='graph-3-tabs',
         figure={
             'data': [{
-                'y':  steel_data_analysis.RandomForest(rate)[2],
+                'y':  test_count,
                 'x': ['Pastry', 'Z-scratch', 'K_Scratch',"Stains",'Dirtiness','Bumps','Other_faults'],
 
                 'type': 'bar',
@@ -106,29 +171,31 @@ def bar_randomforest_test(rate):
 
 
 @app.callback(Output('PR_RF', 'children'),
-              [Input('rate', 'value')])
-def bar_randomforest_predicted(rate):
+              [Input('attributes', 'values'),Input('hours', 'value')])
+def bar_randomforest_predicted(attributes,rate):
+    global cnf,accuracy,test_count,pred_count
     return html.Div([dcc.Graph(
         id='graph-4-tabs',
         figure={
             'data': [{
-                'y':  steel_data_analysis.RandomForest(rate)[3],
+                'y':  pred_count,
                 'x': ['Pastry', 'Z-scratch', 'K_Scratch',"Stains",'Dirtiness','Bumps','Other_faults'],
 
                 'type': 'bar',
-                "name" : "Predicted Results"
+                "name" : "Test Results"
             }]
         }
     )])
 
 @app.callback(Output('TR_KNN', 'children'),
-              [Input('rate', 'value')])
-def bar_knn_test(rate):
+              [Input('attributes', 'values'),Input('rate', 'value')])
+def bar_knn_test(attributes,rate):
+    global cnf_knn,accuracy_knn,test_count_knn,pred_count_knn
     return html.Div([dcc.Graph(
         id='graph-5-tabs',
         figure={
             'data': [{
-                'y':  steel_data_analysis.Kneighbors(rate)[2],
+                'y':  test_count_knn,
                 'x': ['Pastry', 'Z-scratch', 'K_Scratch',"Stains",'Dirtiness','Bumps','Other_faults'],
                 'type': 'bar',
                 "name" : "Test Results"
@@ -138,24 +205,20 @@ def bar_knn_test(rate):
 
 
 @app.callback(Output('PR_KNN', 'children'),
-              [Input('rate', 'value')])
-def bar_knn_predicted(rate):
+              [Input('attributes', 'values'),Input('rate', 'value')])
+def bar_knn_predicted(attributes,rate):
+    global cnf_knn,accuracy_knn,test_count_knn,pred_count_knn
     return html.Div([dcc.Graph(
         id='graph-6-tabs',
         figure={
             'data': [{
-                'y':  steel_data_analysis.Kneighbors(rate)[3],
+                'y':  pred_count_knn,
                 'x': ['Pastry', 'Z-scratch', 'K_Scratch',"Stains",'Dirtiness','Bumps','Other_faults'],
                 'type': 'bar',
                 "name" : "Predicted Results"
             }]
         }
     )])
-
-@app.callback(Output('amount-per-week', 'children'),
-              [Input('rate', 'value')])
-def compute_amount(rate):
-    return steel_data_analysis.Kneighbors(rate)[1]
 
 
 if __name__ == '__main__':
--- steel_data_analysis.py
+++ steel_data_analysis.py
@@ -23,16 +23,19 @@ sns.countplot(x="Class", data= df)
 
 df.loc[df['Class'] == 1, 'V34'] = 0
 df.loc[df['Class'] == 2, 'V34'] = 1
-
-
-X_train, X_test, y_train, y_test = train_test_split(df.iloc[:,:-8], df[['V28','V29','V30','V31','V32','V33','V34']] ,test_size=0.3)
 count=0
 cl_count_test={'Pastry':count,'Z_Scratch':count, 'K_Scatch':count, 'Stains':count,'Dirtiness':count, 'Bumps':count,'Other_Faults':count}
 cl_count_predicted={'Pastry':count,'Z_Scratch':count, 'K_Scatch':count, 'Stains':count,'Dirtiness':count, 'Bumps':count,'Other_Faults':count}
 classes={'V28':'Pastry','V29':'Z_Scratch','V30':'K_Scatch','V31':'Stains','V32':'Dirtiness','V33':'Bumps','V34':'Other_Faults'}
 
+def split_data(features=['V1','V2','V3','V4','V5','V6','V7']):
 
-def RandomForest(estimators=10):
+    X_train, X_test, y_train, y_test = train_test_split(df[features], df[['V28','V29','V30','V31','V32','V33','V34']] ,test_size=0.3)
+    return X_train,X_test,y_train,y_test
+
+
+def RandomForest(attributes,estimators=10):
+    X_train,X_test,y_train,y_test=split_data(attributes)
     clf = RandomForestClassifier(n_estimators = estimators)
     clf.fit(X_train, y_train)
 
@@ -40,7 +43,7 @@ def RandomForest(estimators=10):
     clf.fit(X_train,y_train)
     y_predicted=clf.predict(X_test)
     #print(y2_predicted,y_test)
-    print("Accuracy_Score=",sk.metrics.accuracy_score(y_test,y_predicted))
+    #print("Accuracy_Score=",sk.metrics.accuracy_score(y_test,y_predicted))
     #print("Feature Importances",clf.feature_importances_)
     imp_features=clf.feature_importances_
     imp_features=imp_features[:27]
@@ -51,14 +54,15 @@ def RandomForest(estimators=10):
     y_predicted_df=pd.DataFrame(y_predicted,columns=y_test.columns)
     cnf_matrix = get_cnf_matrix(y_test,y_predicted_df)
     test_count,predicted_count=get_bar_data(y_test,y_predicted_df)
-    
+
     return cnf_matrix,sk.metrics.accuracy_score(y_test,y_predicted),test_count,predicted_count
 
     #plt.bar(x=y_pos,height=imp_features,width=0.8)
     #plt.xticks(y_pos,imp_feature_names)
 
 
-def Kneighbors(k=10):
+def Kneighbors(attributes,k=10):
+    X_train,X_test,y_train,y_test=split_data(attributes)
     clf= KNeighborsClassifier(n_neighbors=k)
     clf.fit(X_train,y_train)
     y_predicted=clf.predict(X_test)
@@ -77,13 +81,11 @@ def get_bar_data(y_test,y_predicted_df):
     for i in range(28,35):
         index='V'+str(i)
         test_class=(y_test[index]==1).sum() #y_test[y_test[[index]]==1].sum()
-    
+
         cl_count_test[classes[index]]= test_class
-        
+
         predicted_class=(y_predicted_df[index]==1).sum()
         cl_count_predicted[classes[index]]= predicted_class
-        
-        
-    return list(cl_count_test.values()),list(cl_count_predicted.values())
 
-print(RandomForest(1))
+
+    return list(cl_count_test.values()),list(cl_count_predicted.values())
